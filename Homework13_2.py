a9cf1e9ea23d6e931fdede03b0568e6f75d7fa17
CuteChipmunk <katerina131299@gmail.com>
2025-01-22 20:51:57 +0300

    Add files via upload

---
+++ Homework13_2.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+ip = "XXX"
+bot = Bot(token=ip)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands=["start"])
+async def hi_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
